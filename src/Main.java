4e93e866cb49802037dd6247a99b12ec4770129e
Alan Souza <alan.barbosa@techsolutio.com>
2023-04-03 17:00:20 -0300

    Imersão java - consumindo uma API top 250 filmes do momento

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        //fazer conexão HTTP e buscar os top 250 filmes
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_uu6o83mv";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+
+
+        //extrair só os dados que nos interessam
+        var parse = new JsonParse();
+        List<Map<String, String>> listFilm = parse.parse(body);
+        for (Map<String, String> filme : listFilm) {
+
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+
+    }
+
+
+
+
+}
